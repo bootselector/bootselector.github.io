d05a3f02a53cea86fd2acfa0bd862dc20a82576f
bootselector <bootselector@yahoo.co.jp>
2026-02-16 11:53:05 +0900

    篠井富屋連峰アップ

--- js/map.js
+++ js/map.js
@@ -220,7 +220,7 @@ function initialize() {
 	}
 
 	// 山アイコン
-	var kmlLayer = new google.maps.KmlLayer('https://bootselector.github.io/kml/mountain.kml?ver=332',
+	var kmlLayer = new google.maps.KmlLayer('https://bootselector.github.io/kml/mountain.kml?ver=333',
 		{
 		suppressInfoWindows: true,
 		map: map,
